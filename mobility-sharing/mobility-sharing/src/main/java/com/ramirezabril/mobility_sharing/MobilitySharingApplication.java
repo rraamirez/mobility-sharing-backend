99e0acd2c5b13abd8e23ab632519e312509c8252
RAUL RAMIREZ ABRIL <91550811+rraamirez@users.noreply.github.com>
2024-10-07 19:30:33 +0200

    This is starting!!!

---
+++ mobility-sharing/mobility-sharing/src/main/java/com/ramirezabril/mobility_sharing/MobilitySharingApplication.java
@@ -0,0 +1,13 @@
+package com.ramirezabril.mobility_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilitySharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilitySharingApplication.class, args);
+	}
+
+}
