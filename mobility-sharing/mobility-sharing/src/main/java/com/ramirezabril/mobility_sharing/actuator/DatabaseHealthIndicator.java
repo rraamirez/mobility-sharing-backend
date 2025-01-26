fbd1683d68b4f48572c2e986e1fd54647db72c3e
RAUL RAMIREZ ABRIL <91550811+rraamirez@users.noreply.github.com>
2025-01-26 22:31:46 +0100

    disabled security for developing reasons

--- mobility-sharing/mobility-sharing/src/main/java/com/ramirezabril/mobility_sharing/actuator/DatabaseHealthIndicator.java
+++ mobility-sharing/mobility-sharing/src/main/java/com/ramirezabril/mobility_sharing/actuator/DatabaseHealthIndicator.java
@@ -6,29 +6,19 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-/**
- * Health indicator for the database, to be used in readiness and liveness endpoints.
- */
 @Component
 public class DatabaseHealthIndicator implements HealthIndicator {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    /**
-     * Verifies if the database is accessible and returns health status accordingly.
-     *
-     * @return Health status of the database.
-     */
     @Override
     public Health health() {
         try {
             jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-            return Health.up().withDetail("database", "connected").build();
+            return Health.up().withDetail("Database", "connected").build();
         } catch (Exception e) {
-            return Health.down(e).withDetail("database", "not connected").build();
+            return Health.down(e).withDetail("Database", "not connected").build();
         }
     }
-    //http://localhost:8080/actuator/health/readiness
-    //http://localhost:8080/actuator/health/liveness
 }
---
+++ mobility-sharing/mobility-sharing/src/main/java/com/ramirezabril/mobility_sharing/actuator/controller/HolaMundoController.java
@@ -0,0 +1,19 @@
+package com.ramirezabril.mobility_sharing.actuator.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HolaMundoController {
+
+    @GetMapping("/alba")
+    public ResponseEntity<String> alba() {
+        return ResponseEntity.ok("alba");
+    }
+
+
+}
